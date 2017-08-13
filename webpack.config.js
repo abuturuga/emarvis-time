ae80b6dff353c042ff0c74e2848765779fc8ec2f
Buturuga Alexandru Iulian <buturuga.alexandru.iulian@gmail.com>
2017-08-13 22:16:45 +0300

    add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const path = require('path');
+const path = require('path'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
   entry: path.resolve(__dirname, './client/index.js'),
@@ -22,8 +24,17 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
+      },
+      {
+       test: /\.scss$/,
+       use: ExtractTextPlugin.extract({
+         fallback: 'style-loader',
+         use: ['css-loader', 'sass-loader']
+       })
+     }
     ]
-  }
-
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ]
 };
